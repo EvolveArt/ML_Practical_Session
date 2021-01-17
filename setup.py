81a8a2a1976443831e5398dce27228879a048ac5
Evolve <Artevolve@yahoo.com>
2021-01-17 20:11:20 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple benchmark of classifiers for the MNIST dataset.',
+    author='EMSE',
+    license='MIT',
+)
